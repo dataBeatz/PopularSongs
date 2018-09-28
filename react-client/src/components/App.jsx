19649d5d68300bfa65c2b389b81c5fb0e991465c
Kevin Chau <kcheez@Kevins-MacBook-Pro.local>
2018-09-27 20:14:31 -0700

    added end points

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -18,14 +18,14 @@ class App extends React.Component {
       showMore: false,
       artistObj: null
     };
-}
+  }
 
   componentDidMount() {
     var randNum = Math.floor(Math.random() * 100) + 1;
-    this.setState({artistID: randNum});
+    this.setState({artistID: 1});
    
-    //axios.get(`http://localhost:3003/artist/id`,{params: {id: randNum}})
-    axios.get(`/artist/` + randNum)
+    // axios.get(`http://localhost:3003/artist/id`,{params: {id: randNum}})
+    axios.get(`/artist/1`)
       .then(response => {
 
         let data = response.data;
@@ -38,15 +38,18 @@ class App extends React.Component {
         let albumTwo = data.albums[1].songs.map(e => [1,e]);
         let albumThree = data.albums[2].songs.map(e => [2,e]);
         let allSongs = albumOne.concat(albumTwo, albumThree);
-
+        
         allSongs.sort((a,b) => {
-          if (a[1].popularity > b[1].popularity)
+          if (a[1].popularity > b[1].popularity) {
             return -1;
-          if (a[1].popularity < b[1].popularity)
+          }
+          if (a[1].popularity < b[1].popularity) {
             return 1;
+          }
           return 0;
         });
-
+        
+        console.log(allSongs);
         allSongs = allSongs.slice(0,10);
 
         this.setState({popularSongs: allSongs});
@@ -59,7 +62,14 @@ class App extends React.Component {
 
   createListOfSongs () {
     let albumArr = [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10];
-    return this.state.popularSongs.map((e,i) => <Song key={e[1]._id} counter={i+1} albumURL={this.state.albumCovers + albumArr[i] + '.jpg'} library={e[1].library} songName={e[1].name} streams={e[1].streams}/>);
+    return this.state.popularSongs
+      .map((e, i) => <Song 
+        key={e[1]._id} 
+        counter={i+1} 
+        albumURL={this.state.albumCovers + albumArr[i] + '.jpg'} 
+        library={e[1].library} 
+        songName={e[1].name} 
+        streams={e[1].streams}/>);
   }
 
   fiveBestSongs () {
@@ -72,19 +82,24 @@ class App extends React.Component {
       //<div className={"container-fluid popular-songs"}>
       <div className={"container-fluid"} styleName={"popular-songs"}>
         <div className={"row"}> 
-        <div className={"col col-lg-1"}>
-              <h3 styleName={"popular-title"}>Popular</h3>
+          <div className={"col col-lg-1"}>
+            <h3 styleName={"popular-title"}>Popular</h3>
           </div> 
         </div>
       
         {this.state.showMore ? this.createListOfSongs() : this.fiveBestSongs()}
 
         <div className={"row"}>
-            <div className={"col col-lg-1"}></div>
-            <div className={"col"}>
-                <button styleName={"spfy-btn"} className={"mt-5"} type={"button"} onClick={() => {this.setState({showMore: !this.state.showMore})}}>{this.state.showMore ? 'SHOW ONLY 5 SONGS' : 'SHOW 5 MORE'}</button>
-            </div>
-            <div className={"col col-lg-1"}></div>
+          <div className={"col col-lg-1"}></div>
+          <div className={"col"}>
+            <button styleName={"spfy-btn"} 
+              className={"mt-5"} 
+              type={"button"} 
+              onClick={() => { this.setState({showMore: !this.state.showMore}); }}>
+              {this.state.showMore ? 'SHOW ONLY 5 SONGS' : 'SHOW 5 MORE'}
+            </button>
+          </div>
+          <div className={"col col-lg-1"}></div>
         </div>
       </div> 
 
